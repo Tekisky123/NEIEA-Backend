324cf71b9f4341ac5f40c5bac49c53547987c314
Tauhid <dev.tauhid@gmail.com>
2025-07-05 20:20:08 +0530

    feat: Add new fields to Course model and update course creation logic

--- models/Course.js
+++ models/Course.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import validator from 'validator'
 
 const CourseSchema = new mongoose.Schema({
   title: {
@@ -9,7 +10,27 @@ const CourseSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  
+
+  imageUrl: {
+    type: String,
+    validate: {
+      validator: (v) => validator.isURL(v), // Ensures valid URL format
+      message: 'Photo URL must be a valid URL'
+    },
+    default: "https://img.freepik.com/free-photo/learning-education-ideas-insight-intelligence-study-concept_53876-120116.jpg?semt=ais_hybrid&w=740"
+  },
+
+  instructor: {
+    type: String,
+    required: true,
+  },
+
+  level: {
+    type: String,
+    required: true,
+    enum: ["beginner", "intermediate", "advanced"],
+  },
+
   duration: {
     type: String,
     required: true,
